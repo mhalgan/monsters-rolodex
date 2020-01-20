621c7f95d115def9c7cda26ca52595dc9623372b
Glauber Camargo Campos Rocha <gccr92@gmail.com>
2020-01-20 15:17:26 -0300

    Updates

---
+++ precache-manifest.d0b791a11ef7dedc25ad28cbcc5c0b0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a3f0eb24750a2ae9dbed5b43a36a27",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "69da5a9ae174996bc148",
+    "url": "/monsters-rolodex/static/css/main.78a3cb67.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69da5a9ae174996bc148",
+    "url": "/monsters-rolodex/static/js/main.ff8505ab.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
